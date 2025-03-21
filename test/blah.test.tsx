fddb9c4b6c88f2c3e01ffc9685eccd03dbfcc0e7
AndDev <23031429@itcelaya.edu.mx>
2025-03-20 20:39:38 -0600

    Update tests to render multiple examples and improve structure

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,20 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { Example_4, Example_1, Example_2, Example_3 } from '../.';
+import React from 'react';
 
 describe('it', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const { unmount } = render(
+      <div>
+        <h1>Example_1</h1>
+        <Example_1 />
+        <h1>Example_2</h1>
+        <Example_2 />
+        <h1>Example_3</h1>
+        <Example_3 />
+        <h1>Example_4</h1>
+        <Example_4 />
+      </div>
+    );
+    unmount();
   });
 });
